809af53dbfc5c269d6c1084dd4265b2efe0de0ba
Luis Hernandez <abdalaluis2@gmail.com>
2023-03-12 13:20:50 +0000

    fix allowedOrigins

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,14 +3,13 @@ const allowedOrigins = require("./allowedOrigins");
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      //Allow postman or another app to have access to our api
-      callback(null, true); //No error and true to the granted access
+      callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
-  optionsSuccessStatus: 200, //To not to have any problems with different devices like smart tv or so
+  optionsSuccessStatus: 200,
 };
 
 module.exports = corsOptions;
